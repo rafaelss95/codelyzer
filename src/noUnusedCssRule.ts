e7f464d85cd9d64b17a3dea64cb9d4b7e81d62f2
Rafael Santana <rafaelsantana3095@gmail.com>
2019-08-17 19:50:51 -0300

    refactor(no-unused-css): general clean up

--- src/noUnusedCssRule.ts
+++ src/noUnusedCssRule.ts
@@ -1,15 +1,14 @@
-import { ElementAst, EmbeddedTemplateAst, PropertyBindingType, TemplateAst } from '@angular/compiler';
-import * as Lint from 'tslint';
-import * as ts from 'typescript';
+import { ElementAst, PropertyBindingType, TemplateAst } from '@angular/compiler';
+import { IRuleMetadata, Replacement, RuleFailure } from 'tslint';
+import { AbstractRule } from 'tslint/lib/rules';
+import { isPropertyAccessExpression, SourceFile } from 'typescript';
 import { ComponentMetadata, StyleMetadata } from './angular/metadata';
 import { NgWalker, NgWalkerConfig } from './angular/ngWalker';
 import { BasicCssAstVisitor } from './angular/styles/basicCssAstVisitor';
 import { CssAst, CssSelectorAst, CssSelectorRuleAst } from './angular/styles/cssAst';
 import { BasicTemplateAstVisitor } from './angular/templates/basicTemplateAstVisitor';
 import { parseTemplate } from './angular/templates/templateParser';
-import { logger } from './util/logger';
-import { SemVerDSL } from './util/ngVersion';
-import { getComponentDecorator, getDecoratorPropertyInitializer, getSymbolName } from './util/utils';
+import { getDecoratorPropertyInitializer } from './util/utils';
 
 interface Strategy {
   attribute(ast: ElementAst): boolean;
@@ -17,90 +16,96 @@ interface Strategy {
   id(ast: ElementAst): boolean;
 }
 
-const CssSelectorTokenizer = require('css-selector-tokenizer');
-
-const isEncapsulationEnabled = (encapsulation: any) => {
-  if (!encapsulation) {
-    return true;
-  }
-
-  // By default consider the encapsulation disabled
-  if (getSymbolName(encapsulation) !== 'ViewEncapsulation') {
-    return false;
-  }
-
-  const encapsulationType = encapsulation.name.text;
-
-  return /^(Emulated|Native)$/.test(encapsulationType);
-};
+const CSS_SELECTOR_TOKENIZER = require('css-selector-tokenizer');
+const CSSAURON = require('cssauron');
+const DEEP_SELECTOR_PATTERN = /(.*)[\/>]$/; // Matches "/deep/" or ">>>"
+const HOST_SELECTOR_PATTERN = /^:/; // Matches ":host" or ":host-context"
+const PSEUDO_ELEMENT_PATTERN = /::/g; // Matches "after", "::before", "::first-letter", "first-line" or "::selection"
 
 // Initialize the selector accessors
-const lang = require('cssauron')({
-  tag(node: ElementAst) {
-    return (node.name || '').toLowerCase();
+const CSSSAURON_LANGUAGE = CSSAURON({
+  attr(node: ElementAst, attrName: string): string | undefined {
+    const targetAttr = node.attrs.find(a => a.name === attrName);
+
+    return targetAttr ? targetAttr.value : undefined;
   },
-  // We do not support it for now
-  contents(node: ElementAst) {
-    return '';
+  children(node: ElementAst): TemplateAst[] {
+    return node.children;
   },
-  id(node: ElementAst) {
-    return this.attr(node, 'id');
-  },
-  class(node: ElementAst) {
-    const classBindings = (node.inputs || [])
-      .filter(b => b.type === PropertyBindingType.Class)
-      .map(b => b.name)
-      .join(' ');
+  class(node: ElementAst): string {
     const classAttr = node.attrs.find(a => a.name.toLowerCase() === 'class');
+    const classBindings = (node.inputs || [])
+      .filter(input => input.type === PropertyBindingType.Class)
+      .map(input => input.name)
+      .join(' ');
 
     return classAttr ? `${classAttr.value} ${classBindings}` : classBindings;
   },
-  parent(node: any) {
+  id(node: ElementAst): string | undefined {
+    return this.attr(node, 'id');
+  },
+  parent(node: ElementAst & { parentNode: ElementAst }): ElementAst {
     return node.parentNode;
   },
-  children(node: ElementAst) {
-    return node.children;
-  },
-  attr(node: ElementAst, attr: string) {
-    const targetAttr = node.attrs.find(a => a.name === attr);
-
-    return targetAttr ? targetAttr.value : undefined;
+  tag(node: ElementAst): string {
+    return node.name.toLowerCase();
   }
 });
 
-// Visitor which normalizes the elements and finds out if we have a match
-class ElementVisitor extends BasicTemplateAstVisitor {
-  visitElement(ast: ElementAst, fn: any) {
-    fn(ast);
-    ast.children.forEach(c => {
-      if (c instanceof ElementAst) {
-        (c as any).parentNode = ast;
-      }
-      this.visit!(c, fn);
-    });
+const STRATEGIES: Strategy = {
+  attribute(ast: ElementAst): boolean {
+    return (ast.inputs || []).some(input => input.type === PropertyBindingType.Attribute);
+  },
+  class(ast: ElementAst): boolean {
+    return (ast.inputs || []).some(input => /^(className|ngClass)$/.test(input.name));
+  },
+  id(ast: ElementAst): boolean {
+    return (ast.inputs || []).some(input => input.name === 'id');
+  }
+};
+
+// Finds out if selector of given type has been used
+const hasSelector = (node: Record<string, any>, type: string): boolean =>
+  node && (/^selectors?$/.test(node.type) ? (node.nodes || []).some(node => hasSelector(node, type)) : node.type === type);
+
+export class Rule extends AbstractRule {
+  static readonly metadata: IRuleMetadata = {
+    description: "Disallows having an unused CSS rule in the component's stylesheet.",
+    hasFix: true,
+    options: null,
+    optionsDescription: 'Not configurable.',
+    ruleName: 'no-unused-css',
+    type: 'maintainability',
+    typescriptOnly: true
+  };
+
+  static readonly FAILURE_STRING = 'Unused styles';
+
+  apply(sourceFile: SourceFile): RuleFailure[] {
+    const walkerConfig: NgWalkerConfig = { cssVisitorCtrl: CssVisitorCtrl };
+    const walker = new Walker(sourceFile, this.getOptions(), walkerConfig);
+
+    return this.applyWithWalker(walker);
   }
 }
 
-// Finds out if selector of given type has been used
-const hasSelector = (s: any, type: string) => {
-  if (!s) {
-    return false;
+// Visitor which normalizes the elements and finds out if we have a match
+class ElementVisitor extends BasicTemplateAstVisitor {
+  visitElement(ast: ElementAst, fn: Function): any {
+    this.validateElement(ast, fn);
+    super.visitElement(ast, fn);
   }
 
-  return s.type === 'selector' || s.type === 'selectors' ? (s.nodes || []).some(n => hasSelector(n, type)) : s.type === type;
-};
+  private validateElement(ast: ElementAst, fn: Function): void {
+    fn(ast);
 
-const dynamicFilters: Strategy = {
-  id(ast: ElementAst) {
-    return (ast.inputs || []).some(i => i.name === 'id');
-  },
-  attribute(ast: ElementAst) {
-    return (ast.inputs || []).some(i => i.type === PropertyBindingType.Attribute);
-  },
-  class(ast: ElementAst) {
-    return (ast.inputs || []).some(i => i.name === 'className' || i.name === 'ngClass');
+    ast.children.forEach(child => {
+      (child as ElementAst & { parentNode: ElementAst }).parentNode = ast;
+
+      this.visit!(child, fn);
+    });
   }
-};
+}
 
 // Filters elements following the strategies:
 // - If has selector by id and any of the elements has a dynamically set id we just skip it.
@@ -109,111 +114,84 @@ const dynamicFilters: Strategy = {
 class ElementFilterVisitor extends BasicTemplateAstVisitor {
   shouldVisit(ast: ElementAst, strategies: Strategy, selectorTypes: object): boolean {
     return (
-      Object.keys(strategies).every(s => {
-        const strategy = strategies[s];
-        return !selectorTypes[s] || !strategy(ast);
-      }) &&
-      (ast.children || []).every(
-        c =>
-          (ast instanceof ElementAst && this.shouldVisit(c as ElementAst, strategies, selectorTypes)) ||
-          (ast instanceof EmbeddedTemplateAst &&
-            (ast.children || []).every(c => this.shouldVisit(c as ElementAst, strategies, selectorTypes)))
-      )
+      Object.keys(strategies).every(strategyKey => !selectorTypes[strategyKey] || !strategies[strategyKey](ast)) &&
+      (ast.children || []).every(child => this.shouldVisit(child as ElementAst, strategies, selectorTypes))
     );
-  }
-}
-
-export class Rule extends Lint.Rules.AbstractRule {
-  static readonly metadata: Lint.IRuleMetadata = {
-    ruleName: 'no-unused-css',
-    type: 'maintainability',
-    description: "Disallows having an unused CSS rule in the component's stylesheet.",
-    options: null,
-    optionsDescription: 'Not configurable.',
-    typescriptOnly: true,
-    hasFix: true
-  };
-
-  apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    const walkerConfig: NgWalkerConfig = { cssVisitorCtrl: CssVisitorCtrl };
-    const walker = new Walker(sourceFile, this.getOptions(), walkerConfig);
-
-    return this.applyWithWalker(walker);
   }
 }
 
 class CssVisitorCtrl extends BasicCssAstVisitor {
   templateAst!: TemplateAst;
 
-  constructor(
-    sourceFile: ts.SourceFile,
-    originalOptions: Lint.IOptions,
-    context: ComponentMetadata,
-    protected style: StyleMetadata,
-    templateStart: number
-  ) {
-    super(sourceFile, originalOptions, context, style, templateStart);
-  }
+  visitCssSelector(ast: CssSelectorAst): boolean {
+    if (!this.templateAst) return true;
 
-  visitCssSelectorRule(ast: CssSelectorRuleAst) {
-    try {
-      const match = ast.selectors.some(s => this.visitCssSelector(s));
-      if (!match) {
-        // We need this because of eventual source maps
-        const {
-          end: { offset: endOffset },
-          start: { offset: startOffset }
-        } = ast;
-        // length + 1 because we want to drop the '}'
-        const length = endOffset - startOffset + 1;
-        this.addFailureAt(startOffset, length, 'Unused styles', Lint.Replacement.deleteText(startOffset - 1, length + 1));
-      }
-    } catch (e) {
-      logger.error(e);
-    }
-    return true;
-  }
+    let parts = '';
 
-  visitCssSelector(ast: CssSelectorAst) {
-    const parts: string[] = [];
-    for (let i = 0; i < ast.selectorParts.length; i += 1) {
-      const c = ast.selectorParts[i];
-      c.strValue = c.strValue.split('::').shift()!;
-      // Stop on /deep/ and >>>
-      if (c.strValue.endsWith('/') || c.strValue.endsWith('>')) {
-        parts.push(c.strValue);
+    for (const selectorPart of ast.selectorParts) {
+      const strValue = (selectorPart.strValue = selectorPart.strValue.split(PSEUDO_ELEMENT_PATTERN)[0]);
+      const isDeepSelector = DEEP_SELECTOR_PATTERN.test(strValue);
+
+      if (isDeepSelector) {
+        parts = parts.concat(strValue, ' ');
         break;
-      } else if (!c.strValue.startsWith(':')) {
-        // skip :host
-        parts.push(c.strValue);
+      }
+
+      const isHostSelector = HOST_SELECTOR_PATTERN.test(strValue);
+
+      if (!isHostSelector) {
+        parts = parts.concat(strValue, ' ');
       }
     }
-    if (!parts.length || !this.templateAst) {
-      return true;
-    }
-    const strippedSelector = parts.map(s => s.replace(/\/|>$/, '').trim()).join(' ');
+
+    if (parts.length === 0) return true;
+
     const elementFilterVisitor = new ElementFilterVisitor(this.getSourceFile(), this._originalOptions, this.context, 0);
-    const tokenized = CssSelectorTokenizer.parse(strippedSelector);
-    const selectorTypesCache = Object.keys(dynamicFilters).reduce((a, key) => {
+    const tokenized = CSS_SELECTOR_TOKENIZER.parse(parts);
+    const selectorTypesCache = Object.keys(STRATEGIES).reduce<Record<string, boolean>>((a, key) => {
       a[key] = hasSelector(tokenized, key);
+
       return a;
     }, {});
 
-    if (!elementFilterVisitor.shouldVisit(this.templateAst as ElementAst, dynamicFilters, selectorTypesCache)) {
+    if (!elementFilterVisitor.shouldVisit(this.templateAst as ElementAst, STRATEGIES, selectorTypesCache)) {
       return true;
     }
 
+    const visitor = new ElementVisitor(this.getSourceFile(), this._originalOptions, this.context, 0);
     let matchFound = false;
+
     const selector = (element: ElementAst) => {
-      if (lang(strippedSelector)(element)) {
+      if (CSSSAURON_LANGUAGE(parts)(element)) {
         matchFound = true;
         return true;
       }
+
       return false;
     };
-    const visitor = new ElementVisitor(this.getSourceFile(), this._originalOptions, this.context, 0);
+
     visitor.visit!(this.templateAst, selector);
+
     return matchFound;
+  }
+
+  visitCssSelectorRule(ast: CssSelectorRuleAst): any {
+    this.validateCssSelectorRule(ast);
+    super.visitCssSelectorRule(ast);
+  }
+
+  private validateCssSelectorRule(ast: CssSelectorRuleAst): void {
+    const match = ast.selectors.some(s => this.visitCssSelector(s));
+
+    if (match) return;
+
+    const {
+      end: { offset: endOffset },
+      start: { offset: startOffset }
+    } = ast;
+    const length = endOffset - startOffset + 1;
+
+    this.addFailureAt(startOffset, length, Rule.FAILURE_STRING, Replacement.deleteText(startOffset - 1, length + 1));
   }
 }
 
@@ -221,73 +199,54 @@ class CssVisitorCtrl extends BasicCssAstVisitor {
 class Walker extends NgWalker {
   private templateAst!: TemplateAst;
 
-  visitClassDeclaration(declaration: ts.ClassDeclaration) {
-    const d = getComponentDecorator(declaration);
-    if (d) {
-      const meta = <ComponentMetadata>this._metadataReader!.read(declaration);
-      this.visitNgComponent(meta);
-      if (meta.template && meta.template.template) {
-        try {
-          const ElementAstCtr = ElementAst as any;
-          SemVerDSL.gte('4.0.0-beta.8', () => {
-            this.templateAst = new ElementAstCtr(
-              '*',
-              [],
-              [],
-              [],
-              [],
-              [],
-              [],
-              false,
-              [],
-              parseTemplate(meta.template!.template.code),
-              0,
-              null,
-              null
-            );
-          }).else(() => {
-            this.templateAst = new ElementAstCtr(
-              '*',
-              [],
-              [],
-              [],
-              [],
-              [],
-              [],
-              false,
-              parseTemplate(meta.template!.template.code),
-              0,
-              null,
-              null
-            );
-          });
-        } catch (e) {
-          logger.error('Cannot parse the template', e);
-        }
-      }
-    }
-    super.visitClassDeclaration(declaration);
+  protected visitNgComponent(metadata: ComponentMetadata): void {
+    this.validateComponent(metadata);
+    super.visitNgComponent(metadata);
   }
 
-  protected visitNgStyleHelper(style: CssAst, context: ComponentMetadata, styleMetadata: StyleMetadata, baseStart: number) {
+  protected visitNgStyleHelper(style: CssAst, context: ComponentMetadata, styleMetadata: StyleMetadata, baseStart: number): void {
     this.validateStyles(style, context, styleMetadata, baseStart);
     super.visitNgStyleHelper(style, context, styleMetadata, baseStart);
   }
 
-  private validateStyles(style: CssAst, context: ComponentMetadata, styleMetadata: StyleMetadata, baseStart: number) {
-    if (!style) {
-      return;
-    }
+  private validateComponent(metadata: ComponentMetadata): void {
+    if (!metadata.template || !metadata.template.template) return;
 
-    const file = this.getContextSourceFile(styleMetadata.url!, styleMetadata.style.source!);
-    const visitor = new CssVisitorCtrl(file, this._originalOptions, context, styleMetadata, baseStart);
+    this.templateAst = new ElementAst(
+      '*',
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      false,
+      [],
+      parseTemplate(metadata.template.template.code),
+      0,
+      null!,
+      null
+    );
+  }
+
+  private validateStyles(style: CssAst, context: ComponentMetadata, styleMetadata: StyleMetadata, baseStart: number): void {
+    const visitor = new CssVisitorCtrl(this.getSourceFile(), this._originalOptions, context, styleMetadata, baseStart);
+    const encapsulationValue = getDecoratorPropertyInitializer(context.decorator, 'encapsulation');
+    const isEncapsulationEnabled =
+      !encapsulationValue || (isPropertyAccessExpression(encapsulationValue) && encapsulationValue.name.text !== 'None');
+
     visitor.templateAst = this.templateAst;
-    const d = getComponentDecorator(context.controller)!;
-    const encapsulation = getDecoratorPropertyInitializer(d, 'encapsulation');
-    if (isEncapsulationEnabled(encapsulation)) {
-      style.visit(visitor);
-      // tslint:disable-next-line:deprecation
-      visitor.getFailures().forEach(f => this.addFailure(f));
-    }
+    style.visit(visitor);
+
+    if (!isEncapsulationEnabled) return;
+
+    visitor.getFailures().forEach(ruleFailure => {
+      const endPositionNumber = ruleFailure.getEndPosition().getPosition();
+      const startPositionNumber = ruleFailure.getStartPosition().getPosition();
+      const strFailure = ruleFailure.getFailure();
+      const fix = ruleFailure.getFix();
+
+      this.addFailureFromStartToEnd(startPositionNumber, endPositionNumber, strFailure, fix);
+    });
   }
 }
